adda2ae2a17a8de0a6a63747ba7c545f2cb8e995
Mark Strempel <mark.strempel@codecool.com>
2023-10-24 15:49:22 +0200

    Add functions for encoding words in the one hot format.

--- workshop/workshopAi.js
+++ workshop/workshopAi.js
@@ -1,12 +1,53 @@
 import { methodNames } from "../data/dictionaries.js"
 import { windows } from "../tensorflow/data.js"
+import * as tf from '@tensorflow/tfjs-node';
+
 
 const names = await methodNames()
 
-const windowSize = 10
+const windowSize = 5
 
-const testWindows = Array.from(windows(5, "helloworld"))
-const allWindows = names.flatMap(name => Array.from(windows(10, name)))
+const allWindows = names.flatMap(name => Array.from(windows(windowSize, name)))
+
+const pairs = allWindows.map(window => [window.toLowerCase(), window])
+
+function collectLetters(pairs) {
+    const allLetters = new Set(pairs.flatMap(([lowerCase, origin]) => [...Array.from(lowerCase), ...Array.from(origin)]))
+    return allLetters
+}
+
+function labelLetters(letters) {
+    const labelByLetter = {}
+
+    let currentIndex = 0
+
+    for (const letter of letters) {
+        labelByLetter[letter] = currentIndex
+        currentIndex = currentIndex + 1
+    }
+
+    return labelByLetter
+}
+
+const labelByLetter = labelLetters(collectLetters(pairs))
+
+function encodeWord(word, labelByLetter) {
+    const numberOfLabels = Object.keys(labelByLetter).length
+    const buffer = tf.buffer([word.length, numberOfLabels])
+
+    for (let letterIndex = 0; letterIndex < word.length; letterIndex++) {
+        const currentLetter = word[letterIndex];
+
+        for (let labelIndex = 0; labelIndex < numberOfLabels; labelIndex++) {
+            buffer.set(labelByLetter[currentLetter] === labelIndex ? 1 : 0, letterIndex, labelIndex)
+        }
+    }
+
+    return buffer.toTensor()
+}
+
+console.log(labelByLetter, encodeWord("abaaa", labelByLetter))
+
 
 // easy make a neural network
 // second step train
