ab56ac91e46317134cbedc66a5b84906968f711e
Mark Strempel <mark.strempel@codecool.com>
2023-10-30 16:29:20 +0100

    Added dictionary model to converter script.

--- converter.js
+++ converter.js
@@ -2,12 +2,17 @@ import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 import { convertToCamelcase as convertToCamelcaseString2String } from './tensorflow/string2String/converter.js';
 import { convertToCamelcase as convertToCamelcaseString2Indices } from './tensorflow/string2Indices/converter.js';
+import { convertToCamelCaseDictionary } from './explicitmodel/camelCaser.js';
+import { englishWords } from './data/dictionaries.js';
 
 const rl = readline.createInterface({ input, output });
 
+const setOfEnglishWords = await englishWords()
+
 const models = {
     "string2string": convertToCamelcaseString2String,
-    "string2indices": convertToCamelcaseString2Indices
+    "string2indices": convertToCamelcaseString2Indices,
+    "dictionaryModel": (input) => convertToCamelCaseDictionary(setOfEnglishWords, input)
 }
 
 
