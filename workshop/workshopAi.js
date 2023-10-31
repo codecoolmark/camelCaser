ef45743f47215f82cb07b464468f1931df9bc6b1
Mark Strempel <mark.strempel@codecool.com>
2023-10-31 15:55:16 +0100

    Add further encoding functions and model to workshop script.

--- workshop/workshopAi.js
+++ workshop/workshopAi.js
@@ -30,25 +30,80 @@ function labelLetters(letters) {
 }
 
 const labelByLetter = labelLetters(collectLetters(pairs))
+const numberOfLabels = Object.keys(labelByLetter).length
 
-function encodeWord(word, labelByLetter) {
+function encodeWords(words, wordSize, labelByLetter) {
     const numberOfLabels = Object.keys(labelByLetter).length
-    const buffer = tf.buffer([word.length, numberOfLabels])
 
-    for (let letterIndex = 0; letterIndex < word.length; letterIndex++) {
-        const currentLetter = word[letterIndex];
+    const buffer = tf.buffer([words.length, wordSize, numberOfLabels])
 
-        for (let labelIndex = 0; labelIndex < numberOfLabels; labelIndex++) {
-            buffer.set(labelByLetter[currentLetter] === labelIndex ? 1 : 0, letterIndex, labelIndex)
+    for (let wordIndex = 0; wordIndex < words.length; wordIndex++) {
+        const word = words[wordIndex]
+
+        for (let letterIndex = 0; letterIndex < wordSize; letterIndex++) {
+            const currentLetter = word[letterIndex];
+    
+            for (let labelIndex = 0; labelIndex < numberOfLabels; labelIndex++) {
+                buffer.set(labelByLetter[currentLetter] === labelIndex ? 1 : 0, wordIndex, letterIndex, labelIndex)
+            }
+        }
+
+    }
+
+    return buffer.toTensor()
+}
+
+function encodeUppercaseIndices(words, wordSize) {
+    const buffer = tf.buffer([words.length, wordSize])
+
+    for (let wordIndex = 0; wordIndex < words.length; wordIndex++) {
+        const word = words[wordIndex]
+
+        for (let characterIndex = 0; characterIndex < wordSize; characterIndex++) {
+            const character = word[characterIndex]
+
+            buffer.set(character === character.toUpperCase() ? 1 : 0, wordIndex, characterIndex)
         }
     }
 
     return buffer.toTensor()
 }
 
-console.log(labelByLetter, encodeWord("abaaa", labelByLetter))
+async function decodeUppercaseIndices(tensor, words, wordSize) {
+    const tensorData = await tensor.array();
 
+    return words.map((word, wordIndex) => 
+        Array.from(word).map((char, characterIndex) => 
+        tensorData[wordIndex][characterIndex] > 0.5 ? char.toUpperCase(): char).join(""))
+}
+
+const hiddenSize = 100;
 
 // easy make a neural network
+const model = tf.sequential();
+
+model.add(tf.layers.dense({
+    inputShape: [windowSize, numberOfLabels],
+    units: hiddenSize
+}))
+
+model.add(tf.layers.dense({
+    units: hiddenSize * 2
+}))
+
+model.add(tf.layers.dense({
+    units: windowSize,
+    activation: "sigmoid"
+}))
+
+model.compile({
+    loss: 'binaryCrossentropy',
+    optimizer: tf.train.adam(),
+    metrics: ['accuracy'],
+})
+
+model.summary()
+
+
 // second step train
 // third step enjoy
